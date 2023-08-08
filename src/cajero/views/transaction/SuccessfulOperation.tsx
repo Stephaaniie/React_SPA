ede880dc5be98bb33e1fc55aaaa086faea3e9ae1
Castillo <s.castillo.laiza@accenture.com>
2023-08-08 10:46:57 -0300

    Se Agregar un setTimeout de 10 segundos a la vista de operacion realizada.

--- src/cajero/views/transaction/SuccessfulOperation.tsx
+++ src/cajero/views/transaction/SuccessfulOperation.tsx
@@ -1,11 +1,22 @@
 import { Alert, AlertTitle, Stack, Typography, Box } from '@mui/material';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const SuccessfulOperation: React.FC = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            navigate("auth/login", { replace: true });
+        }, 10000);
+        return () => clearTimeout(timeout);
+    }, [navigate]);
+
     return (
         <Box sx={{ padding: 8 }}>
             <Stack sx={{ width: '100%' }} spacing={2} alignItems={'center'}>
                 <Alert severity="success">
-                    <AlertTitle>Extracción</AlertTitle>
+                    <AlertTitle>title</AlertTitle>
                     <Typography>
                         Su extracción de monto $500 en la cuenta N° XX-XXX-XXX fue realizado con <strong>éxito</strong>.
                     </Typography>
